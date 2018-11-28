bf43d5c36aee7ca57995b26b468d7e53e4d9b6cd
Causse <romain.causse@gmail.com>
2018-11-28 10:49:26 +0100

    Static version

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
+import { Container } from 'semantic-ui-react'
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Events manager</h1>        
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
     );
   }
